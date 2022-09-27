50d32e8c4026d6477ba54ae6221fc34316686ade
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-09-27 16:23:26 +0300

    my first homework

--- src/Task6.java
+++ src/Task6.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Task6 {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        float x = in.nextFloat();
+        float y = in.nextFloat();
+        if ((y<2-x*x)&((y>x)||((y<x)&(y>0))))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
     }
 }
