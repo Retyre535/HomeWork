4b1c052e6e8c1c71260e2e964691f5f83c60066d
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-09-30 17:59:55 +0300

    my second homework

--- src/Task_2.java
+++ src/Task_2.java
@@ -6,14 +6,14 @@ public class Task_2 {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         for (i = 1; i <= N; ++i) {
-            if (rabbits/wolfs<10)//-волк
-                wolfs-=wolfs*10/rabbits;
-            if (i % 2 != 0){//зайцы*3
+            if (i % 2 == 1){//зайцы*3
                 rabbits *= 3;
                 if (rabbits>19000000)
                     rabbits=19000000;
             }
             if (i % 2 == 0) {//-10зайцев
+                if (rabbits/wolfs<10)//-волк
+                    wolfs-=wolfs*10/rabbits;
                 rabbits -= 10*wolfs;
                 eaten+=10*wolfs;
             }
@@ -21,6 +21,13 @@ public class Task_2 {
                 wolfs+=eaten/70;
                 eaten=eaten-eaten/70*70;
             }
+            if (rabbits<0){
+                if (i+2<=N)
+                    wolfs=0;
+                rabbits=0;
+                break;}
+            if (wolfs<0)
+                wolfs=0;
         }
         System.out.println(rabbits+" "+wolfs);
     }
