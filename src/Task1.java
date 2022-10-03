eef196372c8eb820814f857c62a19c9da70ea0b2
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-10-03 20:53:41 +0300

    my second homework

---
+++ src/Task1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int b=0;
+        int a = in.nextInt();
+        int n = in.nextInt();
+        while (a>0){
+            if(a%10!=n) {
+                b += a % 10;
+                b *= 10;
+            }
+            a/=10;
+        }
+        while (b>0){
+            a+=b%10;
+                a*=10;
+            b/=10;
+        }
+        System.out.println(a/10);
+    }
+}
