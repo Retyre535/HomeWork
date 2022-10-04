d10fb1ebbc124016b41bf6ce0d3b1d73a6b6d77d
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-10-04 17:17:15 +0300

    my third homework

--- src/Task4_2.java
+++ src/Task4_2.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Task4_2 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt(),l=1000000000;
+        int[] n = new int[i];
+        for (int j=0;j<i;++j)
+            n[j] = in.nextInt();
+        for (int y=0;y<i;++y){
+            if (n[y]>l)
+                System.out.println(n[y]);
+            l = n[y];
+        }
     }
 }
