8a4babb2dba5ea61ca21b92d53bb2f2d78de125b
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-10-04 17:18:28 +0300

    my third homework

--- src/Task2.java
+++ src/Task2.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-
 public class Task2 {
     public Task2() {
     }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -13,13 +11,11 @@ public class Task2 {
             n = Math.abs(n);
             b = -1;
         }
-
         while(n > 0) {
             a += n % 10;
             a *= 10;
             n /= 10;
         }
-
         System.out.println(a / 10 * b);
     }
 }
