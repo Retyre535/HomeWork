5f2153a9808a6fb7dbf7a48e2d460a8c2dc84d51
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-10-11 20:58:45 +0300

    my fifth homework

--- src/Task3.java
+++ src/Task3.java
@@ -4,9 +4,11 @@ public class Task3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int c = 5;
+        int c = 1;
         int[][] a = new int[n][n];
-        int x=0, y=n-1;
+        int x=0, y=0;
+        a[x][y]=c;
+        for (int j = n; j>1; j--) a[x][++y] = ++c;
         for (int i=n-1;i>=0;i--) {
             if (i % 2 == 1) {
                 for (int j = i; j > 0; j--) a[--x][y] = ++c;
@@ -16,10 +18,6 @@ public class Task3 {
                 for (int j = i; j > 0; j--) a[x][--y] = ++c;
             }
         }
-        c=1;
-        x=0;
-        y=0;
-        for (int j = n; j>0; j--) a[x][y++] = c++;
         for (int i=0;i<n;i++) {
             for (int j = 0; j < n; j++) {
                 if (a[i][j] < 10) System.out.print(" " + a[i][j] + "\t");
