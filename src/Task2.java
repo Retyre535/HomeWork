7ba7347d5aca5dcc871626490b2972441880371b
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-10-11 20:53:08 +0300

    my fifth homework

--- src/Task2.java
+++ src/Task2.java
@@ -8,37 +8,17 @@ public class Task2 {
         int c = 0;
         int[][] a = new int[d][d];
         int x=n, y=n;
-        a[x][y]=0;
+        a[x][y]=c;
         for (int i=1;i<d;i++) {
             if (i % 2 == 1) {
-                for (int j = 0; j < i; j++) {
-                    c++;
-                    x--;
-                    a[x][y] = c;
-                }
-                for (int j = 0; j < i; j++) {
-                    c++;
-                    y--;
-                    a[x][y] = c;
-                }
+                for (int j = 0; j < i; j++) a[--x][y] = ++c;
+                for (int j = 0; j < i; j++) a[x][--y] = ++c;
             } else {
-                for (int j = 0; j < i; j++) {
-                    c++;
-                    x++;
-                    a[x][y] = c;
-                }
-                for (int j = 0; j < i; j++) {
-                    c++;
-                    y++;
-                    a[x][y] = c;
-                }
+                for (int j = 0; j < i; j++) a[++x][y] = ++c;
+                for (int j = 0; j < i; j++) a[x][++y] = ++c;
             }
         }
-        for (int j = 0; j < d-1; j++) {
-            c++;
-            x--;
-            a[x][y] = c;
-        }
+        for (int j = 0; j < d-1; j++) a[--x][y] = ++c;
         for (int i=0;i<d;i++) {
             for (int j = 0; j < d; j++) {
                 if (a[i][j] < 10) System.out.print(" " + a[i][j] + "\t");
--- src/Task3.java
+++ src/Task3.java
@@ -3,5 +3,29 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int c = 5;
+        int[][] a = new int[n][n];
+        int x=0, y=n-1;
+        for (int i=n-1;i>=0;i--) {
+            if (i % 2 == 1) {
+                for (int j = i; j > 0; j--) a[--x][y] = ++c;
+                for (int j = i; j > 0; j--) a[x][++y] = ++c;
+            } else {
+                for (int j = i; j > 0; j--) a[++x][y] = ++c;
+                for (int j = i; j > 0; j--) a[x][--y] = ++c;
+            }
+        }
+        c=1;
+        x=0;
+        y=0;
+        for (int j = n; j>0; j--) a[x][y++] = c++;
+        for (int i=0;i<n;i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] < 10) System.out.print(" " + a[i][j] + "\t");
+                else System.out.print(a[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
