5af620bf8d5c30e5df5e5a5e752d40b0038b5f8f
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-10-22 23:30:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        byte[] a=new byte[16];
+        for(int i=0;i<16;++i){
+            boolean b = in.nextBoolean();
+            if (b) a[i]=1;
+            else a[i]=0;
+        }
+        for (int i=0;i<16;++i) {
+            if (a[i]==1) System.out.println(true);
+            else System.out.println(false);
+        }
     }
 }
