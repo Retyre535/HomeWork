d13d7b7e548bdb8b2ee763af6451fdbaed45890c
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-11-07 23:21:55 +0300

    Delete Carp.java

--- src/Carp.java
+++
@@ -1,6 +0,0 @@
-public class Carp extends Animals {
-    public void gurgling_talk(){
-        System.out.println("gurgle I'm "+kind+". gurgle I live in "+habitat+".gurgle I can gurgle and"+ movement+". I'm "+food+" gurgle.");
-        System.out.println();
-    }
-}
