a4b673698a8cbd2d7ff535196a546c93f28fff48
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-12-03 22:39:20 +0300

    Delete Lion.java

--- src/Lion.java
+++
@@ -1,6 +0,0 @@
-public class Lion extends Animals {
-    public void growling_talk(){
-        System.out.println("growl I'm "+kind+".growl growl I live in "+habitat+"growl. I can "+ movement+".growl I'm "+food+" growl.");
-        System.out.println();
-    }
-}
