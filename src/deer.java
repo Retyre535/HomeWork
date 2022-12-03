6e4dd5e1d8905907f0d23284e3d297623c6e61df
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2022-12-03 22:39:00 +0300

    Delete deer.java

--- src/deer.java
+++
@@ -1,6 +0,0 @@
-public class deer extends Animals {
-    public void horned_talk(){
-        System.out.println("I'm "+kind+". I live in "+habitat+". I can "+ movement+". I'm "+food+" and i have horns.");
-        System.out.println();
-    }
-}
